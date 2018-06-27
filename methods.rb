c2a11a243a73e0af89647fa92dc7c290d52559da
Gary Boyd <eponick@gmail.com>
2018-06-27 16:02:22 -0400

    test my rotate shifts 1 element, pass

--- methods.rb
+++ methods.rb
@@ -1,5 +1,16 @@
 class Array
     def my_rotate(rot=1)
-        [1]
+        cur_index = 0
+        newarr = Array.new
+        while cur_index < self.length
+            newpos = cur_index + rot
+            if newpos >= self.length
+                newpos -= self.length
+            end
+            newarr[newpos] = self[cur_index]
+            cur_index += 1
+        end
+
+        newarr
     end
 end

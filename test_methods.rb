d112e9da19b989bc96916159391944237f95baff
Gary Boyd <eponick@gmail.com>
2018-06-27 15:43:25 -0400

    test my rotate returns array, fail

--- test_methods.rb
+++ test_methods.rb
@@ -5,4 +5,9 @@ class TestMethods < Minitest::Test
     def test_assert_1_equals_1
         assert_equal(1, 1)
     end
+
+    def test_my_rotate_returns_array
+        t = []
+        assert_equal(Array, t.my_rotate().class)
+    end
 end
